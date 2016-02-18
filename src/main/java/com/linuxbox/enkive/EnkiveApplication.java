8fe9484879480e6d0def6149b4fc0b16f9bbf967
Lee Dobryden <lndobryden@gmail.com>
2016-02-17 21:00:38 -0500

    Detect OSX on startup and use docker default IP for dependencies as a convenience

--- src/main/java/com/linuxbox/enkive/EnkiveApplication.java
+++ src/main/java/com/linuxbox/enkive/EnkiveApplication.java
@@ -2,12 +2,19 @@ package com.linuxbox.enkive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
+@EnableElasticsearchRepositories(basePackages = "com/linuxbox/enkive")
 public class EnkiveApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EnkiveApplication.class, args);
+		SpringApplication app = new SpringApplication(EnkiveApplication.class);
+		String OSName = System.getProperty("os.name");
+		if (OSName.contains("OS X")) {
+			app.setAdditionalProfiles("osx");
+		}
+		app.run(args);
 	}
 
 }
