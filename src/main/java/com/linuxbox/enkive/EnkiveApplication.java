e8c8e09ebcda91d2afb867083e794f5f314ff666
Lee Dobryden <lndobryden@gmail.com>
2016-04-29 19:20:18 -0400

    Remove unnecessary osx specific code

--- src/main/java/com/linuxbox/enkive/EnkiveApplication.java
+++ src/main/java/com/linuxbox/enkive/EnkiveApplication.java
@@ -1,20 +1,30 @@
 package com.linuxbox.enkive;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableElasticsearchRepositories(basePackages = "com/linuxbox/enkive")
+@EnableScheduling
 public class EnkiveApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(EnkiveApplication.class);
-		String OSName = System.getProperty("os.name");
-		if (OSName.contains("OS X")) {
-			app.setAdditionalProfiles("osx");
-		}
-		app.run(args);
+		SpringApplication.run(EnkiveApplication.class, args);
+	}
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	@Bean
+	public Queue enkiveQueue() {
+		//Create a Queue named enkive, mark it durable
+		return new Queue("enkive", true);
 	}
 
 }
